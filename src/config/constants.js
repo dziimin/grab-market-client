6993d5fb0e9dbae38a2c8fccc8b6e18031c7fb1b
Jimin Lee <jmncrk04@gmail.com>
2025-02-13 21:17:25 +0900

    vercel 배포준비, api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,7 @@
 //export const API_URL = "http://localhost:8080";
-export const API_URL = "https://grab-market-server-jm0403.fly.dev";
+//export const API_URL = "https://grab-market-server-jm0403.fly.dev";
+
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-jm0403.fly.dev"
+    : "http://localhost:8080";
