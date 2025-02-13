6834a8dbbeb22f0314687ae839c2bec7c994d3e5
Jimin Lee <jmncrk04@gmail.com>
2025-02-13 20:39:54 +0900

    0213 코드 추가가

--- src/main/index.js
+++ src/main/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import "./index.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs"; // 날짜 가공 라이브러리
+import relativeTime from "dayjs/plugin/relativeTime"; // fromNow() 사용 위해
+import { API_URL } from "../config/constants.js";
+import { Carousel } from "antd";
+
+dayjs.extend(relativeTime);
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
       .get(
-        "https://612454ab-178d-4293-8332-ba86306a4bea.mock.pstmn.io/products"
+        `${API_URL}/products` // "https://612454ab-178d-4293-8332-ba86306a4bea.mock.pstmn.io/products"
       )
       .then(function (result) {
         const products = result.data.products;
@@ -17,33 +24,61 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러 발생 : ", error);
       });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
+      });
   }, []);
 
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" />
-      </div>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageURL}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
       <h1>판매되는 상품들</h1>
       <div id="product-list">
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {/* jsx 문법 */}
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link className="product-link" to={`/product/${product.id}`}>
                 <div>
-                  <img className="product-img" src={product.imageURL} />
+                  <img
+                    className="product-img"
+                    src={`${API_URL}/${product.imageURL}`}
+                  />
                 </div>
                 <div className="product-contents">
                   <span className="product-name">{product.name}</span>
                   <span className="product-price">
                     {product.price.toLocaleString("ko-KR")}원
                   </span>
-                  <div className="product-seller">
-                    <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
-                    />
-                    <span>{product.seller}</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                    <span className="product-date">
+                      {dayjs(product.createdAt).fromNow()}
+                    </span>
                   </div>
                 </div>
               </Link>
--- src/product/index.js
+++ src/product/index.js
@@ -2,33 +2,63 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constants.js";
+import dayjs from "dayjs"; // 날짜 가공 라이브러리
+import { Button, message } from "antd";
 
 function ProductPage() {
   //const prams = useParams(); 를 desturcturing (jsx 문법)
   const { id } = useParams();
   const [product, setProduct] = useState(null);
 
+  const getProduct = () => {
+    axios
+      .get(`${API_URL}/purchase/${id}`)
+      .then(function (result) {
+        setProduct(result.data.product);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   useEffect(function () {
     // useEffect 렌더링 될때 한번만 호출되게끔
     axios
-      .get(
-        `https://612454ab-178d-4293-8332-ba86306a4bea.mock.pstmn.io/products/${id}`
-      )
+      .get(`${API_URL}/products/${id}`)
       .then(function (result) {
-        setProduct(result.data);
+        setProduct(result.data.product);
       })
       .catch(function (error) {
         console.error(error);
       });
   }, []); // [] 처음 렌더링 시에만 호출
 
+  useEffect(function () {
+    getProduct();
+  }, []);
+
   if (product === null) {
-    return <h1>상픔 정보를 받고 있습니다...</h1>;
+    return <h1 id="product-headline">상품 정보를 받고 있습니다...</h1>;
   }
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다.");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
+
   return (
     <div>
       <div id="image-box">
-        <img src={"/" + product.imageURL} />
+        {/* <img src={"/" + product.imageURL} />2025.02.12 수정 */}
+        <img src={`${API_URL}/${product.imageURL}`} />
       </div>
       <div id="profile-box">
         <img src="/images/icons/avatar.png" />
@@ -37,8 +67,20 @@ function ProductPage() {
       <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price.toLocaleString("ko-KR")}원</div>
-        <div id="createdAt">2025년 2월 4일</div>
-        <div id="despcription">{product.description}</div>
+        <div id="createdAt">
+          {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+        </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          재빨리 구매하기
+        </Button>
+        <pre id="despcription">{product.description}</pre> {/* pre : 줄바꿈 */}
       </div>
     </div>
   );
